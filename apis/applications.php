757e274f23d5a083ca942900cccf1f31ba9771b4
mse-lunani <michael.se-lunani@riarauniversity.ac.ke>
2024-04-30 19:37:24 +0300

    first commit

--- apis/applications.php
+++ apis/applications.php
@@ -1,6 +1,7 @@
 <?php
 include_once 'operations.php';
 $m['status'] = 0;
+$m['data'] = [];
 $id = security("job_id");
 $sql = "SELECT applications.*, job_seekers.name FROM applications
         JOIN job_seekers ON applications.applicant_id = job_seekers.id
@@ -10,4 +11,5 @@ if(count($result) > 0){
     $m['status'] = 1;
     $m['data'] = $result;
 }
+
 echo json_encode($m);
--- apis/getJobs2.php
+++ apis/getJobs2.php
@@ -1,11 +1,8 @@
 <?php
 include_once 'operations.php';
 $m['status'] = 0;
-$m['data'] = [];
-$id = security("employer_id");
 $sql = "SELECT vacancy.*, employer.agency_name FROM vacancy 
         JOIN employer ON vacancy.employer_id = employer.id
-        where employer_id = '$id'
         ";
 $result = select_rows($sql);
 if(count($result) > 0){
