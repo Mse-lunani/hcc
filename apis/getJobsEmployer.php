34faccd4f3a043f1124da8ec452b6ab7b8d34fc4
mse-lunani <michael.se-lunani@riarauniversity.ac.ke>
2024-05-01 08:38:05 +0300

    first commit

---
+++ apis/getJobsEmployer.php
@@ -0,0 +1,14 @@
+<?php
+include_once 'operations.php';
+$m['status'] = 0;
+$id = security("employer_id");
+$sql = "SELECT vacancy.*, employer.agency_name FROM vacancy 
+        JOIN employer ON vacancy.employer_id = employer.id
+        where vacancy.employer_id = '$id'
+        ";
+$result = select_rows($sql);
+if(count($result) > 0){
+    $m['status'] = 1;
+    $m['data'] = $result;
+}
+echo json_encode($m);
